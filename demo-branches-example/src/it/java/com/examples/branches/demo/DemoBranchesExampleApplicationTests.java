2d5f356f8bb6d3685e1cd48547a4cdb4f8ea7b2c
alessandro <alessandro.feri@stud.unifi.it>
2020-11-16 13:20:55 +0100

    fixed default integration test to make the build succeed

--- demo-branches-example/src/it/java/com/examples/branches/demo/DemoBranchesExampleApplicationTests.java
+++ demo-branches-example/src/it/java/com/examples/branches/demo/DemoBranchesExampleApplicationTests.java
@@ -1,13 +1,16 @@
 package com.examples.branches.demo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoBranchesExampleApplicationTests {
+public class DemoBranchesExampleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
